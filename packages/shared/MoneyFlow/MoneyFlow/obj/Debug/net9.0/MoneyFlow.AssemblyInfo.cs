589bddf592c8200610e7ed30061811579c74432d
Guilherme Moreira <guilhermedasilvamoreira0@gmail.com>
2025-09-22 15:02:40 -0300

    ajuste de pastas

--- packages/shared/MoneyFlow/MoneyFlow/obj/Debug/net9.0/MoneyFlow.AssemblyInfo.cs
+++ packages/shared/MoneyFlow/MoneyFlow/obj/Debug/net9.0/MoneyFlow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoneyFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9506c201ab881b7ac0f20c93a90d369047e2ffcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96f572474a96a10c817c8cd910d74e5fb0f4e587")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoneyFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoneyFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
